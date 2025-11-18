7cb95ae578bdaeefca50cbbf17ed448b3b704839
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-11-18 15:56:26 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "board.h"
+ 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	srand((unsigned)time(NULL)); 
+	
+	printf("************************************\n");
+	printf("************************************\n");
+	printf("             GAME START             \n");
+	printf("************************************\n");
+	printf("************************************\n");
+	
+//step1. initiallization(player name setting, variables)
+
+//step2. turn play (do-while)
+//2-1.status printing
+//2-2. roll die
+//2-3. move (result)
+//2-4. change turn, shark move 
+
+//step3. game end(winner printing)
+	
+	
+	
+	printf("************************************\n");
+	printf("************************************\n");
+	printf("         !!CONGRATULATION!!         \n");
+	printf("               THE END              \n");
+	printf("************************************\n");
+	printf("************************************\n");
+	
+	return 0;
+}
