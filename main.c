168944db46fffc4f6865bea5f213711929d6b2a3
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-11-18 16:28:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,12 @@
 #include "board.h"
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define MAX_DIE		6
 
+int rolldie(void)
+{
+	return rand()%MAX_DIE + 1;
+}
 int main(int argc, char *argv[]) {
 	
 	srand((unsigned)time(NULL)); 
@@ -16,13 +21,27 @@ int main(int argc, char *argv[]) {
 	printf("************************************\n");
 	
 //step1. initiallization(player name setting, variables)
-
+board_initboard();
 //step2. turn play (do-while)
+int cnt = 0;
+int pos;
+do{
+	int die_result;
+	int coinResult;
 //2-1.status printing
+	board_printBoardStatus();
 //2-2. roll die
+	die_result = rolldie();
 //2-3. move (result)
+	pos += die_result;
+	printf("pos : %i (die:%i\n)", pos, die_result);
+	
+	coinResult += board_getBoardCoin(pos);
+	printf("coin : %i\n",coinResult);	
+	
 //2-4. change turn, shark move 
-
+	cnt++;
+}while(cnt<5);
 //step3. game end(winner printing)
 	
 	
