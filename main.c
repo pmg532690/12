a981e058c8f586c7c44d8beed5222387600efab7
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-11-26 00:34:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,117 @@
 #include "board.h"
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define MAXLENGTH	30
 #define MAX_DIE		6
+#define N_PLAYER	3
 
+#define PLAYERSTATUS_LIVE		0
+#define PLAYERSTATUS_DIE 		1
+#define PLAYERSTATUS_END		2
+
+char player_name[N_PLAYER][MAXLENGTH];
+int player_position[N_PLAYER];
+int player_coin[N_PLAYER];
+int player_status[N_PLAYER];
+char player_statusString[3][MAXLENGTH] = {"LIVE", "DIE", "END "};
+
+void printPlayerPosition(int player)
+{
+	int i;
+	
+	for (i=0;i<N_BOARD;i++)
+	{
+		printf("|");
+		if (i == player_position[player])
+		{
+			printf("%c", player_name[player][0]);
+		}
+		else
+		{
+			if (board_getBoardStatus(i) == BOARDSTATUS_OK)
+				printf(" ");
+			else
+				printf("X");
+				
+		}
+		#if 0
+		if (board_status[i] == BOARDSTATUS_NOK)
+			printf("X");
+		else
+			printf("O");
+			#endif
+	}
+	printf("|\n");
+
+	
+		
+}
+
+void printPlayerStatus(void)
+{
+	int i;
+	for (i=0;i<N_PLAYER;i++)
+	{
+		printf("%s : pos %i, coin %i, status %s\n",
+				player_name[i],
+				player_position[i],
+				player_coin[i],
+				player_statusString[player_status[i]]);
+		printPlayerPosition(i);
+	}
+}
+
+void initPlayer(void)
+{
+	int i;
+	for (i=0;i<N_PLAYER;i++)
+	{
+		
+		player_position[i] = 0;
+		player_coin[i] = 0;
+		player_status[i] = PLAYERSTATUS_LIVE;
+		printf("Player %i's name: ", i);
+		scanf("%s", player_name[i]);
+		fflush(stdin);
+		
+	}
+}
+
+int gameEnd(void)
+{	
+	int i;
+	int flag_end = 1;
+	for (i=0;i<N_PLAYER;i++)
+	{
+		if(player_status[i] == PLAYERSTATUS_LIVE)
+			flag_end = 0;
+	}
+	
+	return flag_end;
+}
+void checkDie(void)
+{
+	int i;
+	
+	for (i=0;i<N_PLAYER;i++)
+	{
+		if (board_getBoardStatus(player_position[i]) == BOARDSTATUS_NOK)
+		{
+		
+			player_status[i] = PLAYERSTATUS_DIE;
+			printf("So Sad! %s died at position %i\n" ,player_name[i], player_position[i]);
+		}
+	}
+}
 int rolldie(void)
 {
 	return rand()%MAX_DIE + 1;
 }
 int main(int argc, char *argv[]) {
+	int cnt;
+	int turn;
+
+	int dum;
 	
 	srand((unsigned)time(NULL)); 
 	
@@ -22,28 +126,78 @@ int main(int argc, char *argv[]) {
 	
 //step1. initiallization(player name setting, variables)
 board_initBoard();
+initPlayer();
+//player init
+
 //step2. turn play (do-while)
-int cnt = 0;
-int pos;
+cnt = 0;
+turn = 0;
+
 do{
 	int die_result;
-	int coinResult;
-//2-1.status printing
+	if (player_status[turn] != PLAYERSTATUS_LIVE)
+	{
+		turn = (turn + 1)%N_PLAYER;
+		continue;
+	}
+	
+
+	//2-1.status printing
 	board_printBoardStatus();
-//2-2. roll die
-	die_result = rolldie();
+	printPlayerStatus();
+	//player status(각자) 
+	//2-2. roll die
+	printf("%s turn!!", player_name[turn]);
+	printf("Press any key to roll a die!\n");
+	scanf("%d",&dum);
+	fflush(stdin);	
+	die_result = rolldie(); //player turn
 //2-3. move (result)
-	pos += die_result;
-	printf("pos : %i (die:%i\n)", pos, die_result);
+	player_position[turn] += die_result;
+	if (player_position[turn] >= N_BOARD - 1)
+	{
+		player_position[turn] = N_BOARD -1;
+		player_status[turn] = PLAYERSTATUS_END;
+	}
 	
-	coinResult += board_getBoardCoin(pos);
-	printf("coin : %i\n",coinResult);	
+	printf("Die result : %i, %s moved to %i\n",
+				 die_result, 
+				 player_name[turn],
+				 player_position[turn]);
+
+	player_coin[turn] += board_getBoardCoin(player_position[turn]);
+	printf("Lucky! %s got %i coins\n",player_name[turn],player_coin[turn]);	
+
 	
-//2-4. change turn, shark move 
+//2-4. change turn
+	turn = (turn + 1) % N_PLAYER;
+	
+	//sharkmove
+	if(turn == 0)
+	{
+		int shark_pos = board_stepShark();
+		printf("Shark moves to %i\n", shark_pos);
+		
+		checkDie();
+	}
 	cnt++;
-}while(cnt<5);
+}while(gameEnd() == 0);
+
 //step3. game end(winner printing)	
-	
+	int i;
+    int winner = 0; 
+    
+
+    for (i = 1; i < N_PLAYER; i++) {
+        if (player_coin[i] > player_coin[winner]) {
+            winner = i; 
+        }
+    }
+    
+    printf("\n--- 최종 결과 ---\n");
+	printPlayerStatus();
+    printf("\n?? **게임 종료! 축하합니다!**\n");
+    printf("최종 우승자는 **%s** 입니다! (획득 코인: %i)\n", player_name[winner], player_coin[winner]);	
 	
 	
 	printf("************************************\n");
